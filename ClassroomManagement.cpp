65ae9b9fda289305bc9900c210abbf0a9e1ae491
vaidikBhongade <211135@iiitt.ac.in>
2022-12-01 00:05:17 +0530

    Teacher_class commit

--- ClassroomManagement.cpp
+++ ClassroomManagement.cpp
@@ -3,7 +3,57 @@ using namespace std;
 
 class Student{};
 
-class Teacher{};
+class Teacher{
+    private:
+        int ID;
+        string name;
+        string email;
+        string password;
+    
+    public:
+        Teacher(){ }
+        Teacher(int ID, string name, string email, string password);
+
+        int getID(){ return ID;}
+        string getName(){ return name;}
+        string getEmail(){ return email;}
+        string getPassword(){ return password;}
+        string getDomainAddress(){
+            string domain_address = email;
+            int pos = domain_address.find("@");
+            domain_address = domain_address.substr(pos + 1);
+            pos = domain_address.find(".");
+            domain_address = domain_address.substr(0,pos);
+            return domain_address;
+        }
+        void changePassword(){
+            string old_password;
+            string new_password;
+            cout << "Enter old password : ";
+            cin >> old_password;
+            cout << "Enter new password : ";
+            cin >> new_password;
+            while(1){
+                if(old_password == password){
+                    password = new_password;
+                    cout << "Password Changed.\n";
+                    break;
+                }
+                cout << END_LINE << endl;
+                cout << "Incorrect password, please enter again : ";
+                cin >> old_password;
+                cout << "Enter new password : ";
+                cin >> new_password;
+            }
+        }
+
+        friend ofstream& operator << (ofstream& fout, Teacher &teacher);
+        friend ifstream& operator << (ifstream& fin, Teacher &teacher);
+        friend ofstream& operator << (ofstream& cout, Teacher &teacher);
+        
+        ~Teacher(){};
+
+};
 
 class Classroom{};
 
